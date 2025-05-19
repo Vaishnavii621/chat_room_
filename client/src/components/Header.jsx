8c149b81753ce0c846f9528780525f54cae99a31
Vaishnavii621 <136267681+Vaishnavii621@users.noreply.github.com>
2025-05-20 03:04:23 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,8 @@ import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import { BsBoxArrowLeft } from "react-icons/bs";
 import io from "socket.io-client";
-const socket = io(import.meta.env.VITE_SOCKET_URL);
+//const socket = io(import.meta.env.VITE_SOCKET_URL);
+const socket = io.connect("http://localhost:5000");
 
 const Header = () => {
   const navigate = useNavigate();
