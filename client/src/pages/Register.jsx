fb3a2397793eb6d118bc0a04a62e32f5fa0e1656
Vaishnavii621 <136267681+Vaishnavii621@users.noreply.github.com>
2025-05-20 03:13:20 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,7 +12,7 @@ export const Register = () => {
  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5001/api/v1/register", {
+      const res = await axios.post("http://localhost:5000/api/v1/register", {
         username,
         password,
       });
